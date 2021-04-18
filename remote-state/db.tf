74c1803e7ead935158d17c8023b498e2a93373d6
Severyn Matsiak <44036021+severko22@users.noreply.github.com>
2021-04-18 23:47:11 +0300

    Added remote state

---
+++ remote-state/db.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
---
+++ remote-state/s3-bucket.tf
@@ -0,0 +1,37 @@
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "1.22.0"
+  bucket  = "sev-terraform-state"
+  acl     = "private"
+
+  object_lock_configuration = {
+    object_lock_enabled = "Enabled"
+    rule = {
+      default_retention = {
+        mode = "GOVERNANCE"
+        days = 1
+      }
+    }
+  }
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  versioning = {
+    enabled = "true"
+  }
+
+  create_bucket = true
+  force_destroy = true
+
+  # S3 bucket-level Public Access Block configuration
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
